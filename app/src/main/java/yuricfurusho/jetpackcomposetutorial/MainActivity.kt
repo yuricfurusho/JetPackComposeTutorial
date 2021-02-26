748e1dc2b9308edacd9f9c40b34ecd7b451cb7dd
Yuri Furusho <yuri.cooperfurusho@traderev.com>
2021-02-25 20:40:08 -0500

    1st

---
+++ app/src/main/java/yuricfurusho/jetpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,38 @@
+package yuricfurusho.jetpackcomposetutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import yuricfurusho.jetpackcomposetutorial.ui.theme.JetPackComposeTutorialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetPackComposeTutorialTheme {
+        Greeting("Android")
+    }
+}
