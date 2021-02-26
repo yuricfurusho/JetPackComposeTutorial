3089de4f5939f00a47d184e1c42cd94fd7127728
Yuri Furusho <yuri.cooperfurusho@traderev.com>
2021-02-26 11:30:47 -0500

    Lesson 2: Step 4.3: Styles the Image

--- app/src/main/java/yuricfurusho/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/yuricfurusho/jetpackcomposetutorial/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,8 +28,12 @@ class MainActivity : AppCompatActivity() {
     private fun NewStory() {
         Column(modifier = Modifier.padding(16.dp)) {
             Image(
-                    painter = painterResource(R.drawable.header),
-                    contentDescription = null
+                painter = painterResource(R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
             )
             Text("A day in Shark Fin Cove")
             Text("Davenport, California")
