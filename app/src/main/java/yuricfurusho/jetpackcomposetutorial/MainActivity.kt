c5c40cf909f76f4ecf271014127871532741ea89
Yuri Furusho <yuri.cooperfurusho@traderev.com>
2021-02-25 20:53:57 -0500

    Lesson 1: Step 4: Preview Composable Functions

--- app/src/main/java/yuricfurusho/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/yuricfurusho/jetpackcomposetutorial/MainActivity.kt
@@ -2,13 +2,10 @@ package yuricfurusho.jetpackcomposetutorial
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
-import yuricfurusho.jetpackcomposetutorial.ui.theme.JetPackComposeTutorialTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +18,11 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun Greeting(name: String) {
         Text(text = "Hello $name!")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        Greeting("Android")
     }
 }
