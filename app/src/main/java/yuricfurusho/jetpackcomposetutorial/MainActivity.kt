6049c36581e291d8d536bb4d08f01a99c2295c2e
Yuri Furusho <yuri.cooperfurusho@traderev.com>
2021-02-26 09:26:05 -0500

    Lesson 2: Step 3: Modifier.padding modifier

--- app/src/main/java/yuricfurusho/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/yuricfurusho/jetpackcomposetutorial/MainActivity.kt
@@ -3,10 +3,13 @@ package yuricfurusho.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     private fun NewStory() {
-        Column {
+        Column(modifier = Modifier.padding(16.dp)) {
             Text("A day in Shark Fin Cove")
             Text("Davenport, California")
             Text("December 2018")
